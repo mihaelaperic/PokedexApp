fa16f168a545dcaa1117310e36f59752f8debaad
mihaelaperic <mperic8@student.unizd.hr>
2024-01-30 23:21:51 +0100

    snd commit

---
+++ app/src/main/java/org/unizd/rma/peric/pokedex/LoadingActivity.kt
@@ -0,0 +1,27 @@
+package org.unizd.rma.peric.pokedex
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivityList::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
